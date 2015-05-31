17c996ef46806c7ef2e097b92c80f1deafa6b76b
Maomo <mjhmjr@sina.cn>
2015-05-31 22:48:48 +0800

    Don't blame me

---
+++ app/20121379040/js/totop.js
@@ -0,0 +1,13 @@
+$(function(){ 
+$(window).scroll(function(){ 
+var scrollt = document.documentElement.scrollTop + document.body.scrollTop; 
+if( scrollt >200 ){ 
+$("#gotop").fadeIn(400);  
+}else{ 
+$("#gotop").stop().fadeOut(400); 
+} 
+}); 
+$("#gotop").click(function(){  
+$("html,body").animate({scrollTop:"0px"},200); 
+}); 
+});
